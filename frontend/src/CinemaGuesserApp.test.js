8a7813bb48bf3dc9cdbc287a2c4a9b3c8b3c4e69
mshpo <nickshpo@gmail.com>
2022-10-25 00:18:35 -0400

    Create favicon

--- frontend/src/CinemaGuesserApp.test.js
+++ frontend/src/CinemaGuesserApp.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import CinemaGuesser from './CinemaGuesserApp';
+import CinemaGuesserApp from './CinemaGuesserApp';
 
 test('renders learn react link', () => {
   render(<CinemaGuesserApp />);
