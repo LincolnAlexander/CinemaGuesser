3f1d1c1c3d498b4257fe8858c8765df0a98cdf1c
mshpo <nickshpo@gmail.com>
2022-10-27 14:29:31 -0400

    Add nav bar. Style login container.

--- frontend/src/components/LoginContainer.js
+++ frontend/src/components/LoginContainer.js
@@ -35,18 +35,17 @@ const LoginContainer = () => {
       return;
     }
   };
+  
   return (
 
-    // flex flex-col container mx-auto bg-gray-200 border-solid border-black 
-    <div className="flex flex-1 flex-col content-center items-center w-96 lg:w-3/5 max-w-lg bg-pr-black rounded-xl shadow border p-8">
-          <p className='text-lg font-medium'>Username</p>
-          <input className='border rounded border-pr-white p-1 w-64 h-8 text-center' type='text' placeholder='Enter Login' ref={(c) => loginName = c} />
+    <div className="flex justify-center items-start h-full">
+      <div className="flex flex-col content-center items-center w-96 my-28 md:my-36 xl:my-96 mx-2 lg:w-3/5 max-w-lg bg-pr-black rounded-xl shadow border-2 pt-6 pb-8">
+            <input className='bg-pr-black border-b-2 border-pr-yellow focus:border-b-4 focus:outline-0 focus:font-bold mt-8 mb-4 w-64 h-8 placeholder-pr-white text-center text-pr-white' type='text' placeholder='Enter Login' ref={(c) => loginName = c} />
+            <input className='block bg-pr-black border-b-2 border-pr-yellow focus:border-b-4 focus:outline-0 focus:font-bold my-4 w-64 h-8 placeholder-pr-white text-center text-pr-white' type='password' placeholder='Enter Password' ref={(c) => loginPassword = c} />
 
-          <p className='text-lg font-medium'>Password</p>
-          <input className='block border rounded border-pr-white p-1 w-64 h-8 text-center' type='password' placeholder='Enter Password' ref={(c) => loginPassword = c} />
-
-          <button className='block border my-6 rounded-full bg-gradient-to-r from-pr-yellow to-pr-red text-white w-36 h-10 font-medium' type='button' onClick={doLogin}>Login</button>
-          <a className='text-pr-yellow text-center font-medium' href="#" type='button'>Register</a>
+            <button className='block my-6 rounded-full bg-gradient-to-r from-pr-yellow to-pr-red hover:border-2 text-white w-36 h-10 font-medium hover:font-extrabold hover:underline hover: bg-pr-yellow' type='button' onClick={doLogin}>Login</button>
+            <a className='text-pr-yellow text-center font-medium hover:font-extrabold hover:underline hover:text-pr-white' href="#" type='button'>Register</a>
+      </div>
     </div>
   );
 }
