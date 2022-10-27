b4c5d5b170859481e7055154b3d7753216977426
mshpo <nickshpo@gmail.com>
2022-10-26 22:53:57 -0400

    Add custom colors and font to Tailwind config

--- frontend/src/components/LoginContainer.js
+++ frontend/src/components/LoginContainer.js
@@ -1,70 +1,64 @@
 
 import React, { useState } from 'react';
 
-const LoginContainer = () => 
-{
+const LoginContainer = () => {
   var loginName;
-    var loginPassword;
+  var loginPassword;
 
-    const [message,setMessage] = useState('');
+  const [message, setMessage] = useState('');
 
-    const doLogin = async event => 
-    {
-        event.preventDefault();
+  const doLogin = async event => {
+    event.preventDefault();
 
-        var obj = {username:loginName.value,password:loginPassword.value};
-        var js = JSON.stringify(obj);
+    var obj = { username: loginName.value, password: loginPassword.value };
+    var js = JSON.stringify(obj);
 
-        try
-        {    
-            const response = await fetch('http://localhost:5000/api/login',
-                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+    try {
+      const response = await fetch('http://localhost:5000/api/login',
+        { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
 
-            var res = JSON.parse(await response.text());
+      var res = JSON.parse(await response.text());
 
-            if( res.id <= 0 )
-            {
-                setMessage('User/Password combination incorrect');
-            }
-            else
-            {
-                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
-                localStorage.setItem('user_data', JSON.stringify(user));
+      if (res.id <= 0) {
+        setMessage('User/Password combination incorrect');
+      }
+      else {
+        var user = { firstName: res.firstName, lastName: res.lastName, id: res.id }
+        localStorage.setItem('user_data', JSON.stringify(user));
 
-                setMessage('');
-                window.location.href = '/cards';
-            }
-        }
-        catch(e)
-        {
-            alert(e.toString());
-            return;
-        }    
-    };
+        setMessage('');
+        window.location.href = '/cards';
+      }
+    }
+    catch (e) {
+      alert(e.toString());
+      return;
+    }
+  };
   return (
-    
+
     // flex flex-col container mx-auto bg-gray-200 border-solid border-black 
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <div className = "flex-col justify-center items-center">
+    <div className="container mx-auto bg-pr-gray rounded-xl shadow border p-8 m-10">
+      <div className="flex-col justify-center items-center">
         <div className='m-5'>
           <p className='text-lg font-medium'>Username</p>
-          <input className = 'border rounded border-black w-64 h-8' type = 'text' placeholder='Enter Login' ref={(c) => loginName = c}/>
+          <input className='border rounded border-black w-64 h-8' type='text' placeholder='Enter Login' ref={(c) => loginName = c} />
         </div>
         <div className='m-5'>
           <p className='text-lg font-medium'>Password</p>
-          <input className = 'border rounded border-black w-64 h-8' type = 'password' placeholder='Enter Password' ref={(c) => loginPassword = c}/>
+          <input className='border rounded border-black w-64 h-8' type='password' placeholder='Enter Password' ref={(c) => loginPassword = c} />
         </div>
         <div className='m-5'>
-          <button className = 'border rounded-full bg-blue-500 text-white w-36 h-10 font-medium' type = 'button'>Login</button>
+          <button className='border rounded-full bg-blue-500 text-white w-36 h-10 font-medium' type='button'>Login</button>
         </div>
         <div className='m-5'>
-          <button className = 'border rounded-full bg-green-500 text-white w-36 h-10 font-medium' type = 'button' onClick={doLogin}>Register</button>
+          <button className='border rounded-full bg-green-500 text-white w-36 h-10 font-medium' type='button' onClick={doLogin}>Register</button>
         </div>
-        
+
       </div>
-         
-          
-          
+
+
+
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pr: {
+          'black': '#0A0A0A',
+          'gray': '#ACACAC',
+          'red': '#D00000',
+          'white': '#FFFFFF',
+          'yellow': '#F1CF54'
+        }
+      },
+      fontFamily: {
+        roboslab: ['Roboto Slab', 'serif']
+      },
+    },
   },
   plugins: [],
 }
