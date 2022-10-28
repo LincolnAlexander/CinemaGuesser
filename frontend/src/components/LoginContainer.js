e12d18c70d0fc27bb8e6030bb2ca791137e0c6cc
LincolnAlexander <lincoln1662@knights.ucf.edu>
2022-10-28 13:46:35 -0400

    Changed Login UI, added Blobs

--- frontend/src/components/LoginContainer.js
+++ frontend/src/components/LoginContainer.js
@@ -40,10 +40,10 @@ const LoginContainer = () => {
   };
   
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center mt-20'>
       <div className="flex justify-center items-start w-1/2">
-        <div className = "flex flex-col basis-1/2 justify-center items-center ">
-          <form className='mt-20'>
+        <div className = "flex flex-col basis-1/2 justify-center items-center rounded-md bg-slate-500 bg-opacity-10 backdrop-blur-sm">
+          <form className='mt-10'>
             <div className='relative'>
               <input className='peer h-10  border-b-2 border-pr-yellow text-pr-white focus:outline-none bg-transparent placeholder-transparent' id='username' placeholder='a'></input>
               <label className = 'absolute left-0 -top-3.5 text-pr-yellow text-md transtion-all peer-placeholder-shown:text-base peer-placeholder-shown:text-pr-gray peer-placeholder-shown:top-2 peer-focus: -top-3.5  peer-focus: text-md ' htmlFor='username'>Username</label>
@@ -61,10 +61,10 @@ const LoginContainer = () => {
           </div>
               
         </div>
-        <div className='flex basis-1/2 w-full h-full justify-center items-center'>
+        {/* <div className='flex basis-1/2 w-full h-full justify-center items-center '>
           <Blob/>
-          {/* <Circle className=''/> */}
-        </div>
+          
+        </div> */}
       </div>
     </div>
   );
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -5,13 +5,19 @@ import Welcome from '../components/Welcome';
 import {ReactComponent as Blob} from '../images/blob-gradient.svg';
 
 
+
   
 function LoginPage() {
   return (
     <>
     <Welcome></Welcome>
     <LoginContainer></LoginContainer>
-    {/* <Blob className='w-60 '/> */}
+    {/* <Blob className='w-64 absolute bottom-32 inset-x-1/3'/> */}
+    <Blob className='w-64 absolute bottom-12 right-96'/>
+    <Blob className='w-64 absolute right-5 top-24'/>
+    {/* <Blob className='w-96 absolute top-72 right-64'/> */}
+    <Blob className='w-96 absolute inset-x-24'/>
+    <Blob className='w-64 absolute inset-y-24'/>
     </>
     
   );
