0c6d9a4214dd398951cc4212831698a27d812827
mshpo <nickshpo@gmail.com>
2022-11-01 01:24:50 -0400

    Small edits.

--- frontend/src/components/LoginContainer.js
+++ frontend/src/components/LoginContainer.js
@@ -5,6 +5,7 @@ import Register from '../pages/RegisterPage';
 // import RegisterModal from './Modals/RegisterModal';
 import RegisterPage from '../pages/RegisterPage';
 // import LoginPage from '../pages/LoginPage';
+// import { Routes, Route } from 'react-router-dom';
 
 const LoginContainer = () => {
   var loginName;
@@ -17,8 +18,6 @@ const LoginContainer = () => {
 
     var obj = { username: loginName.value, password: loginPassword.value };
     var js = JSON.stringify(obj);
-
-    console.log(js);
 
     try {
       const response = await fetch('http://localhost:5000/api/login', {
@@ -51,61 +50,63 @@ const LoginContainer = () => {
   // const [openRegisterModal, setRegister] = useState(false);
 
   return (
-    <div className='flex justify-center mt-20'>
-      <div className='flex justify-center items-start w-1/2'>
-        <div className='flex flex-col basis-1/2 justify-center items-center rounded-md bg-slate-500 bg-opacity-10 backdrop-blur-sm'>
-          <form className='mt-10'>
-            <div className='relative'>
-              <input
-                className='peer h-10  border-b-2 border-pr-yellow text-pr-white focus:outline-none bg-transparent placeholder-transparent'
-                id='username'
-                placeholder='a'
-              ></input>
-              <label
-                className='absolute left-0 -top-3.5 text-pr-yellow text-md transtion-all peer-placeholder-shown:text-base peer-placeholder-shown:text-pr-gray peer-placeholder-shown:top-2 peer-focus: -top-3.5  peer-focus: text-md '
-                htmlFor='username'
-              >
-                Username
-              </label>
-            </div>
-            <div className='relative mt-8'>
-              <input
-                className='peer h-10 border-b-2 border-pr-yellow text-pr-white focus:outline-none bg-transparent placeholder-transparent'
-                id='password'
-                placeholder='a'
-              ></input>
-              <label
-                className='absolute left-0 -top-3.5 text-pr-yellow text-md transtion-all peer-placeholder-shown:text-base peer-placeholder-shown:text-pr-gray peer-placeholder-shown:top-2 peer-focus: -top-3.5  peer-focus: text-md '
-                htmlFor='password'
-              >
-                Password
-              </label>
-              <button
-                className='transition-all ease-in-out delay-150 duration-300 hover:scale-110 block my-6 rounded-full bg-gradient-to-r from-pr-yellow to-pr-red  text-white w-52 h-10 font-medium hover:font-extrabold '
-                type='button'
-                onClick={doLogin}
-              >
-                Login
-              </button>
-            </div>
-          </form>
-          <div>
-            <span className='text-pr-white pr-2'>Don't have an account?</span>
-            {/* <button className='text-pr-yellow text-center font-medium hover:font-extrabold hover:underline' onClick={() => setRegister(true)}>Register</button> */}
-            {/* <RegisterModal open={openRegisterModal} onClose = {() => setRegister(false)}>
-            </RegisterModal> */}
+    <>
+      <div className='flex justify-center mt-20'>
+        <div className='flex justify-center items-start w-1/2'>
+          <div className='flex flex-col basis-1/2 justify-center items-center rounded-md bg-slate-500 bg-opacity-10 backdrop-blur-sm'>
+            <form className='mt-10'>
+              <div className='relative'>
+                <input
+                  className='peer h-10  border-b-2 border-pr-yellow text-pr-white focus:outline-none bg-transparent placeholder-transparent'
+                  id='username'
+                  placeholder='a'
+                ></input>
+                <label
+                  className='absolute left-0 -top-3.5 text-pr-yellow text-md transtion-all peer-placeholder-shown:text-base peer-placeholder-shown:text-pr-gray peer-placeholder-shown:top-2 peer-focus: -top-3.5  peer-focus: text-md '
+                  htmlFor='username'
+                >
+                  Username
+                </label>
+              </div>
+              <div className='relative mt-8'>
+                <input
+                  className='peer h-10 border-b-2 border-pr-yellow text-pr-white focus:outline-none bg-transparent placeholder-transparent'
+                  id='password'
+                  placeholder='a'
+                ></input>
+                <label
+                  className='absolute left-0 -top-3.5 text-pr-yellow text-md transtion-all peer-placeholder-shown:text-base peer-placeholder-shown:text-pr-gray peer-placeholder-shown:top-2 peer-focus: -top-3.5  peer-focus: text-md '
+                  htmlFor='password'
+                >
+                  Password
+                </label>
+                <button
+                  className='transition-all ease-in-out delay-150 duration-300 hover:scale-110 block my-6 rounded-full bg-gradient-to-r from-pr-yellow to-pr-red  text-white w-52 h-10 font-medium hover:font-extrabold '
+                  type='button'
+                  onClick={doLogin}
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+            <div>
+              <span className='text-pr-white pr-2'>Don't have an account?</span>
+              {/* <button className='text-pr-yellow text-center font-medium hover:font-extrabold hover:underline' onClick={() => setRegister(true)}>Register</button> */}
+              {/* <RegisterModal open={openRegisterModal} onClose = {() => setRegister(false)}>
+              </RegisterModal> */}
 
-            <a
-              className='block text-pr-yellow text-center font-medium hover:font-extrabold hover:underline'
-              href='#'
-              type='button'
-            >
-              Forgot Password?
-            </a>
+              <a
+                className='block text-pr-yellow text-center font-medium hover:font-extrabold hover:underline'
+                href='#'
+                type='button'
+              >
+                Forgot Password?
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
