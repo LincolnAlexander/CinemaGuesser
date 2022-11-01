5c4f937d534652541fe89ed86e7fa1c3f49c7981
mshpo <nickshpo@gmail.com>
2022-11-01 00:33:26 -0400

    Set up Router.

--- frontend/src/CinemaGuesserApp.js
+++ frontend/src/CinemaGuesserApp.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import LoginPage from './pages/LoginPage';
 import Nav from './components/Nav';
-
+import { Routes, Route } from 'react-router-dom';
 
 function CinemaGuesserApp() {
   return (
     <>
       <Nav />
-      <LoginPage />
+      <Routes>
+        {/* Add more paths here */}
+        <Route path='/' element={<LoginPage />} />
+
+        {/* For unknown path go to login page*/}
+        <Route path='*' element={<LoginPage />} />
+      </Routes>
     </>
   );
 }
