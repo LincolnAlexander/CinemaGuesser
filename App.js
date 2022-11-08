104059bd7680259bfdd2245448194f5731f59970
Cristian Gutierrez <Cristiang52001@hotmail.com>
2022-11-07 20:40:43 -0500

    Imported Axios and commented steps to do so

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Button,
+  Alert,
+} from "react-native";
+import { useFonts } from "expo-font";
+import LoginPage from "./pages/LoginPage";
+/*make sure to install axios on mobile
+steps: on terminal type:
+1) npm init -y
+2) npm install axios
+*/
+const axios = require("axios");
 
-import * as React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity, Button, Alert } from 'react-native';
-import { useFonts } from 'expo-font'; 
-import LoginPage from './pages/LoginPage';
-
-const customFont = 'RobotoSlab-Medium';
+const customFont = "RobotoSlab-Medium";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    'RobotoSlab-Medium': require('./assets/fonts/RobotoSlab-Medium.ttf'),
+    "RobotoSlab-Medium": require("./assets/fonts/RobotoSlab-Medium.ttf"),
   });
-  return (
-    
-    <LoginPage/>
-    
-  );
+  return <LoginPage />;
 }
-
-
