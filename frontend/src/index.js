b4f00fca2fd98a7f5f240ec7ed1bee9cf8f0bb50
LincolnAlexander <lincoln1662@knights.ucf.edu>
2022-11-08 16:21:47 -0500

    Changes to GamePage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import CinemaGuesserApp from './CinemaGuesserApp';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import GamePage from './pages/GamePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <CinemaGuesserApp />
+      {/* <GamePage/> */}
     </BrowserRouter>
   </React.StrictMode>
 );
