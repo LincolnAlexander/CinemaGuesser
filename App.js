5b928f8dced0da8c313a55c09fead51e8a9bf27d
Cristian Gutierrez <Cristiang52001@hotmail.com>
2022-11-07 22:40:22 -0500

    install react-native-stack --save

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import * as React from "react";
 import { useFonts } from "expo-font";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-//import { NavigationContainer } from "react-navigation/native";
+import { NavigationContainer } from "react-navigation/native";
 const axios = require("axios");
 
 const customFont = "RobotoSlab-Medium";
@@ -21,6 +21,6 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     "RobotoSlab-Medium": require("./assets/fonts/RobotoSlab-Medium.ttf"),
   });
-  return <RegisterPage />;
-  //return <LoginPage />;
+  //return <RegisterPage />;
+  return <LoginPage />;
 }
