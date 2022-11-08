4e6771b25c8631881ede371b0e5b9c3850b1fe09
Cristian Gutierrez <Cristiang52001@hotmail.com>
2022-11-07 23:41:04 -0500

    dummy

--- App.js
+++ App.js
@@ -14,13 +14,12 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 //import { NavigationContainer } from "react-navigation/native";
 const axios = require("axios");
-
 const customFont = "RobotoSlab-Medium";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     "RobotoSlab-Medium": require("./assets/fonts/RobotoSlab-Medium.ttf"),
   });
-  //return <RegisterPage />;
-  return <LoginPage />;
+  return <RegisterPage />;
+  //return <LoginPage />;
 }
