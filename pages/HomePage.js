50fc1b682fcc0e928b34a1a8953798509a5aaea2
Cristian Gutierrez <Cristiang52001@hotmail.com>
2022-11-12 17:17:10 -0500

    .

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,171 +1,43 @@
-import { AsyncStorage } from "@react-native-async-storage/async-storage";
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ImageBackground,
-  TouchableOpacity,
-  Button,
-  Alert,
-  TextInput,
-} from "react-native";
-import { useFonts } from "expo-font";
-import RegisterPage from "./RegisterPage";
-const customFont = "RobotoSlab-Medium";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import axios from "axios";
-import { createErrorHandler } from "expo/build/errors/ExpoErrorManager";
-import { setStatusBarBackgroundColor } from "expo-status-bar";
-const Stack = createStackNavigator();
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import Button from "../components/Button";
+const HomeScreen = ({ navigation }) => {
+  const [userDetails, setUserDetails] = React.useState();
+  React.useEffect(() => {
+    getUserData();
+  }, []);
 
-const FadeInView = (props) => {
-  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
-
-  useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 10000,
-    }).start();
-  }, [fadeAnim]);
-
-  return (
-    <Animated.View // Special animatable View
-      style={{
-        ...props.style,
-        opacity: fadeAnim, // Bind opacity to animated value
-      }}
-    >
-      {props.children}
-    </Animated.View>
-  );
-};
-
-function Register() {
-  return <RegisterPage />;
-}
-
-const errorHandler = () => {
-  setStatusBarBackgroundColor("red");
-};
-
-const login = async () => {
-  const payload = {
-    username: username,
-    password: password,
+  const getUserData = async () => {
+    const userData = await AsyncStorage.getItem("userData");
+    if (userData) {
+      setUserDetails(JSON.parse(userData));
+    }
   };
 
-  try {
-    const baseURL = "https://cinema-guesser.herokuapp.com/";
-
-    const response = await axios.post(baseURL + "/api/user/login", payload);
-  } catch (error) {
-    console.log(error.response.data);
-    errorHandler;
-  }
-};
-
-const callAPI = () => {
-  axios
-    .get()
-
-    .then(function (response) {
-      alerts(JSON.stringify(response));
-    })
-    .catch(function (error) {
-      alert(error);
-    });
-};
-
-export default function LoginPage() {
-  const [fontsLoaded] = useFonts({
-    "RobotoSlab-Medium": require("../assets/fonts/RobotoSlab-Medium.ttf"),
-  });
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const ref = React.useRef(null);
+  const logout = () => {
+    AsyncStorage.setItem(
+      "userData",
+      JSON.stringify({ ...userDetails, loggedIn: false })
+    );
+    navigation.navigate("LoginScreen");
+  };
 
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.background}
-        source={require("../assets/images/AppBackground.jpg")}
-      >
-        <View style={styles.header}>
-          <Image
-            style={styles.logo}
-            source={require("../assets/images/AppLogo.png")}
-          ></Image>
-        </View>
-      </ImageBackground>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 40,
+      }}
+    >
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+        Welcome {userDetails?.fullname}
+      </Text>
+      <Button title="Logout" onPress={logout} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    flex: 1,
-    marginTop: "15%",
-  },
-  background: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-    alignItems: "center",
-  },
-  loginContainer: {
-    flex: 2,
-    alignItems: "center",
-    justifyContent: "center",
-    // margin: '5%',
-  },
-
-  username: {
-    margin: 15,
-  },
-
-  password: {
-    margin: 15,
-  },
-  text: {
-    color: "white",
-    textAlign: "center",
-    fontFamily: "RobotoSlab-Medium",
-    fontWeight: "500",
-    fontSize: 20,
-  },
-
-  horizontalBar: {
-    backgroundColor: "#F1CF54",
-    height: 3,
-    width: 275,
-  },
-  verticalBar: {
-    backgroundColor: "#F1CF54",
-    height: 20,
-    width: 2,
-  },
-
-  touchables: {
-    flex: 1,
-    alignItems: "center",
-    // justifyContent: 'center',
-  },
-  loginText: {
-    fontWeight: "500",
-    fontSize: 16,
-    textAlign: "center",
-    margin: 10,
-    color: "white",
-    textAlign: "center",
-    fontFamily: "RobotoSlab-Medium",
-  },
-});
+export default HomeScreen;
