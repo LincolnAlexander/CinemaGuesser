8af031c409c5afed873fa1088c527e7d76fcfad1
Cristian Gutierrez <Cristiang52001@hotmail.com>
2022-11-14 17:30:35 -0500

    updated tab logos

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,6 +2,11 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import Button from "../components/Button";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+// <Button title="Logout" onPress={logout} />
 const HomePage = ({ navigation }) => {
   const [userDetails, setUserDetails] = React.useState();
   React.useEffect(() => {
@@ -23,20 +28,58 @@ const HomePage = ({ navigation }) => {
     navigation.navigate("LoginPage");
   };
 
+  function HomePage() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+
+  function SettingsPage() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Settings!</Text>
+        <Button title="Logout" onPress={logout} />
+      </View>
+    );
+  }
+
+  function WatchListPage() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Watch List!</Text>
+      </View>
+    );
+  }
+
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        paddingHorizontal: 40,
-      }}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+          } else if (route.name === "Watch List") {
+            iconName = focused ? "film" : "film-outline";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
     >
-      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-        Welcome {userDetails?.fullname}
-      </Text>
-      <Button title="Logout" onPress={logout} />
-    </View>
+      <Tab.Screen name="Home" component={HomePage} />
+      <Tab.Screen name="Settings" component={SettingsPage} />
+      <Tab.Screen name="Watch List" component={WatchListPage} />
+    </Tab.Navigator>
   );
 };
 
