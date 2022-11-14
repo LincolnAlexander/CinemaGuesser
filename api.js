4a1e2d12d5a9c2b4acfa6636743ef28c8fb79780
Kaeden Le <kaedenle@gmail.com>
2022-11-14 04:50:27 -0500

    Three changes including: * Adding GamesPlayed endpoints * Replacing Update GamesPlayed/Score with op (operations) allowing for updating and addition * Adding Leaderboard endpoint. Sorts users by given field then get the first n users.

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ var sha256 = require('js-sha256');
 //Gaming
 exports.setApp = function ( app, client )
 {
-    
+//-----------------------------------VALIDATION ENDPOINTS-----------------------------------
     app.post('/api/login', async (req, res, next) =>             //login
     {
       // incoming: login, password
@@ -40,8 +40,12 @@ exports.setApp = function ( app, client )
       var error = '';
       const { FirstName, LastName, Login, Pass } = req.body;
       const Password = sha256.hmac('key', req.body.Password);
+      //stats and list(s) for user
       const Score = 0;
+      const GamesPlayed = 0;
+      const WatchList = [];
       const db = client.db();
+      
 
       var err = 'Username Taken';
       var pass = '';
@@ -54,7 +58,7 @@ exports.setApp = function ( app, client )
 
       if(results.length == 0)
       {
-        db.collection('Users').insertOne({FirstName, LastName, Login, Password, Score});
+        db.collection('Users').insertOne({FirstName, LastName, Login, Password, Score, GamesPlayed, WatchList});
         fn = FirstName;
         ln = LastName;
         lgn = Login;
@@ -65,7 +69,112 @@ exports.setApp = function ( app, client )
       var ret = {firstname: fn, lastname: ln, login: lgn, error: err}
       res.status(200).json(ret);
     });
+//-----------------------------------LEADERBOARD ENDPOINTS------------------------------------
+    //leaderboard endpoint that sorts by gamesplayed or score
+    app.post('/api/leaderboard', async (req, res, next) =>         //leaderboard
+    {
+      //REQ: page, per_page(default=10), sortby
+      var per_page = req.body.per_page;
+      if(!req.body.per_page){
+        per_page = 10
+      }
+      const db = client.db();
+      //MongoDB pipeline sort -> project -> skip -> limit
+      var pipeline = [
+        {
+          '$sort': {
+            //define with req.body.sortby below
+          }
+        }, {
+          '$project': {
+            'Login': 1, 
+            'Score': 1, 
+            'GamesPlayed': 1
+          }
+        }, {
+          '$skip': (per_page * req.body.page)
+        }, {
+          '$limit': per_page
+        }
+      ]
+      //build the $sort
+      pipeline[0]['$sort'][req.body.sortby] = 1;
 
+      const results = await db.collection('Users').aggregate(pipeline).toArray();
+      console.log(pipeline);
+      var ret = {list: results};
+      res.status(200).json(ret);
+    });
+//-----------------------------------GAMES PLAYED ENDPOINTS-----------------------------------
+    //get scores (ADJUST FOR JWT TOKEN)
+    app.post('/api/get_games_played', async (req, res, next) =>         //get_games_played
+    {
+      //REQ: login
+      const db = client.db();
+      const{login} = req.body;
+      const results = await db.collection('Users').find({Login:login}).toArray();
+      var err = '';
+      var GamesPlayed;
+      //check if record exists
+      if(results.length == 0)
+      {
+        err = 'no record found';
+      }
+      else
+      {
+        GamesPlayed = results[0].GamesPlayed;
+      }
+      var ret = {gamesplayed: GamesPlayed};
+      res.status(200).json(ret);
+    });
+
+     //perform operations on scores (ADJUST FOR JWT TOKEN)
+     app.post('/api/op_games_played', async (req, res, next) =>         //op_games_played
+     {
+       //REQ: login, value, mode
+       //MODE OPs
+       /*
+       0: set
+       1: add
+       */
+       var mode;
+       var op = '';
+       if(!req.body.mode){
+         mode = 0;
+       }
+       else{
+        mode = req.body.mode;
+       }
+       const db = client.db();
+       const{login} = req.body;
+       const results = await db.collection('Users').find({Login:login}).toArray();
+       var err = '';
+       var GamesPlayed = results[0].GamesPlayed;
+       //check if record exists
+       if(results.length == 0)
+       {
+         err = 'no record found';
+       }
+       else
+       {
+         //set mode
+         if(mode == 0){
+          GamesPlayed = req.body.value;
+          console.log(GamesPlayed);
+          op = 'set';
+         }
+         //add mode
+         else if(mode == 1){
+          GamesPlayed += req.body.value;
+          op = 'add';
+         }
+         
+         await db.collection('Users').updateOne({Login:login}, {$set: { "GamesPlayed" : GamesPlayed}});
+       }
+       var ret = {gamesplayed: GamesPlayed, operation: op};
+       res.status(200).json(ret);
+     });
+//-----------------------------------SCORE ENDPOINTS-----------------------------------
     //get scores (ADJUST FOR JWT TOKEN)
     app.post('/api/get_score', async (req, res, next) =>         //get_score
     {
@@ -88,10 +197,23 @@ exports.setApp = function ( app, client )
       res.status(200).json(ret);
     });
 
-    //update scores (ADJUST FOR JWT TOKEN)
-    app.post('/api/update_score', async (req, res, next) =>         //update_score
+    //perform operations on scores (ADJUST FOR JWT TOKEN)
+    app.post('/api/op_score', async (req, res, next) =>         //op_score
     {
-      //REQ: login, score
+      //REQ: login, value, mode
+      //MODE OPs
+      /*
+      0: set
+      1: add
+      */
+      var mode;
+      var op = ''
+      if(!req.body.mode){
+        mode = 0;
+      }
+      else{
+        mode = req.body.mode;
+       }
       const db = client.db();
       const{login} = req.body;
       const results = await db.collection('Users').find({Login:login}).toArray();
@@ -104,14 +226,23 @@ exports.setApp = function ( app, client )
       }
       else
       {
-        Score = req.body.score;
-        const replace_result = await db.collection('Users').updateOne({Login:login}, {$set: { "Score" : Score}});
-        console.log(replace_result);
+        //set mode
+        if(mode == 0){
+          Score = req.body.value;
+          op = 'set';
+        }
+        //add mode
+        else if(mode == 1){
+          Score += req.body.value;
+          op = 'add';
+        }
+        
+        await db.collection('Users').updateOne({Login:login}, {$set: { "Score" : Score}});
       }
-      var ret = {score: Score};
+      var ret = {score: Score, operation: op};
       res.status(200).json(ret);
     });
-
+//-----------------------------------MOVIE ENDPOINTS-----------------------------------
     //gets random movies
     app.get('/api/movies', async (req, res, next) =>             //movies
     {
