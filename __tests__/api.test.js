c9a15a7003b222fc2aafc11b09d7c7d2e377624d
Iain-Crowe <iaincrowe@knights.ucf.edu>
2022-11-28 19:21:27 -0500

    Updated some tests

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -2,6 +2,7 @@ const { MongoClient } = require('mongodb');
 
 const request = require('supertest');
 var sha256 = require('js-sha256');
+const { EmailAddress } = require('@sendgrid/helpers/classes');
 
 const baseURL = 'http://localhost:5000/api';
 
@@ -12,7 +13,6 @@ describe('POST /login', () => {
         login: 'UnitTestUser',
         password: 'qwerty123*'
     };
-
     const testUserResponse = {
         firstName : 'StaticUser',
         lastName : 'ForTesting',
@@ -54,16 +54,16 @@ describe('POST /login', () => {
     });
 
     // Tests with null user and password
-    //const nullRequest = {
+    // const nullRequest = {
     //    login: null,
     //    password: null
-    //}
+    // }
 
-    //test('/login endpoint with null request', async () => {
+    // test('/login endpoint with null request', async () => {
     //    const res = await request(baseURL).post('/login').send(nullRequest);
-    //
+    
     //    expect(res.statusCode).toBe(200);
-    //});
+    // });
 });
 
 // Testing /register endpoint
@@ -75,61 +75,42 @@ describe('POST /register', () => {
         FirstName: 'Register',
         LastName: 'UnitTest',
         Login: 'RegisterUnitTest',
-        Password: sha256.hmac('key', 'test'),
-        Score: 0,
-        GamesPlayed: 0,
-        WatchList: []
+        Pass: sha256.hmac('key', 'test'),
+        Email: 'registertest@email.com'
     };
     // Connect to db
     beforeAll(async () => {
         //require('dotenv').config();
         //client = new MongoClient(process.env.MONGODB_URI);
-        
-        client.connect();
+
         db = client.db();
-    });
-    
-    // Close connection with db
-    afterAll(async () => {
-        await client.close();
     });
 
     // Registers dummy user
     test('/register endpoint adding dummy user', async () => {
-        // Add via api
         const res = await request(baseURL).post('/register').send(registerTest);
 
         expect(res.statusCode).toBe(200);
+
+        console.log(res);
 
         const data = res.body;
         expect(data.firstname).toBe(registerTest.FirstName);
         expect(data.lastname).toBe(registerTest.LastName);
         expect(data.login).toBe(registerTest.Login);
-        
-        
-        // Manual addition
-        let FirstName = registerTest.FirstName;
-        let LastName = registerTest.LastName;
-        let Login = registerTest.Login;
-        let Password = registerTest.Password;
-        let Score = 0; 
-        let GamesPlayed = 0; 
-        let WatchList = [];
-
-        const Users = db.collection('Users');
-        Users.insertOne({FirstName, LastName, Login, Password, Score, GamesPlayed, WatchList});
+        expect(data.email).toBe(registerTest.Email);
     });
 
     // Removes dummy user and verifies removal
-    test('Test user is removed from data base after test', async () => {
-        const Users = db.collection('Users');
-        const user = await Users.find({Login:registerTest.Login}).toArray();
-        console.log(user);
+    // test('Test user is removed from data base after test', async () => {
+    //     const Users = db.collection('Users');
+    //     const user = await Users.find({Login:registerTest.Login,Password:registerTest.Pass}).toArray();
+    //     console.log(user);
 
-        const result = await Users.deleteOne({Login:registerTest.Login});
-        // Checking for db acknowledgment
-        expect(result.acknowledged).toBeTruthy();
-        // Checking that record was deleted
-        expect(result.deletedCount).toBeGreaterThan(0);
-    });
+    //     const result = await Users.deleteOne({Login:registerTest.Login,Password:registerTest.Pass});
+    //     // Checking for db acknowledgment
+    //     expect(result.acknowledged).toBeTruthy();
+    //     // Checking that record was deleted
+    //     expect(result.deletedCount).toBeGreaterThan(0);
+    // });
 });
