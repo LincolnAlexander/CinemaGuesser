9feb9eec304ad8aa380ba4984b8d0c46aa43660f
mshpo <nickshpo@gmail.com>
2022-11-29 10:20:56 -0500

    Refactore code. Fix style uniformity.

--- frontend/src/pages/EmailCheck.js
+++ frontend/src/pages/EmailCheck.js
@@ -1,22 +1,15 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function EmailCheck() {
   const navigate = useNavigate();
 
-  /*setTimeout(() => {
-    console.log("hi");
-  }, 0);*/
-  function move(){
-    navigate('/');
-  }
-  
-return (
-    <h1 className='mt-52 mx-1 text-pr-yellow font-bold text-center text-xl'>
-        <div>A registration link has been sent to your email! </div>
-        <div onClick={move} style={{cursor: 'pointer'}}>Click here to return to login page.</div>
-    </h1>
-);
-  
-
+  return (
+    <div
+      onClick={() => navigate('/')}
+      className='cursor-pointer mt-52 mx-1 text-pr-yellow font-bold text-center text-xl'
+    >
+      <p>A registration link has been sent to your email! </p>
+      <p>Click here to return to login page.</p>
+    </div>
+  );
 }
