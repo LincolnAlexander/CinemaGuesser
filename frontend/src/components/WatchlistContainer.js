ff808cd653f9e91455fdead045ca56f8bf3daff5
Kaeden Le <kaedenle@gmail.com>
2022-12-06 02:18:00 -0500

    did stuff

--- frontend/src/components/WatchlistContainer.js
+++ frontend/src/components/WatchlistContainer.js
@@ -10,8 +10,6 @@ let [page, setPage] = useState(0);
 const [login, setLogin] = useState(JSON.parse(localStorage.getItem('user_data')).login)
 const [turnOn, setModal] = useState(false);
 
-const[searchMovie, setSearchMovie] = useState('');
-
 const[poster, setPoster] = useState(false);
 const[year, setYear] = useState(false);
 const[title, setTitle] = useState(false);
@@ -173,24 +171,14 @@ function closeRoundModal() {
 
 const capitalize = (str, lower = false) =>
 (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());
-
-
-// Write new api code here
-async function searchList(movie)
-{
-    setSearchMovie(movie);
-    console.log(movie);
-}
+;
 
 return (
     <>
         {/*whole box that leaderboard will lay on*/}
-        <div className='flex flex-col  m-8 md:m-10 min-h-[46rem]'>
-        <div className='flex justify-center'>
-            <input className='w-80 h-8 border-pr-white bg-slate-300 bg-opacity-20  backdrop-blur-sm rounded hover:border-2 focus:border-2 focus:outline-none text-pr-white pl-1' placeholder='Enter Movie to Seach' type = 'search' alt = "seach-bar" onChange={(e)=>{searchList(e.target.value)}} value = {searchMovie}></input>
-        </div>
+        <div className='flex flex-col justify-between m-8 md:m-20 min-h-[46rem]'>
         {/*the actual table*/}
-        <table className='w-screen sm:w-1/6 max-w-2xl bg-slate-500 bg-opacity-10 backdrop-blur-sm rounded-lg self-center mb-4 mt-10'>
+        <table className='w-screen sm:w-1/6 max-w-2xl bg-slate-500 bg-opacity-10 backdrop-blur-sm rounded-lg self-center mb-4'>
             {/*the above header for the table*/}
             <thead className='text-left bg-slate-500 bg-opacity-5 backdrop-blur-sm'>
             {/*Must be used at least once for thead, defines the values of the row*/}
